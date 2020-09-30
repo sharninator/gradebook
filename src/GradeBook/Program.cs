e6dd15cc4ee7532fc22db1971cc24b3f8381a7bf
sharninator <sharnz11@gmail.com>
2020-09-30 15:09:46 +1000

    changes to code

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -7,7 +7,10 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
-          
+            var book = new Book("Sharni's Grade Book");
+            book.AddGrade(89.1);
+            book.AddGrade(90.5);
+        
             var grades = new List<double>() {12.7, 10.3, 6.11, 4.1};
             grades.Add(56.1);
 
@@ -19,17 +22,6 @@ namespace GradeBook
             result /= grades.Count; 
             Console.WriteLine($"The average grade is {result:N1}");
             
-
-
-
-           if (args.Length > 0) 
-            {
-                Console.WriteLine($"Hello {args[0]}!");
-            }
-            else 
-            {
-                Console.WriteLine("Hello!");
-            }
         }
     }
 }
